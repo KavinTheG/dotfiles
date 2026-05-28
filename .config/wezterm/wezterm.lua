7f3e54d8cf1bd65f905e0525cb21c796c3569cd8
Kavin Gnanapandithan <kavin.balag@gmail.com>
2026-05-28 14:58:36 -0400

    Newest config files

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,10 +2,10 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 -- Use a dark colorscheme instead of setting background directly
-config.color_scheme = "Catpuccin Latte"
+-- config.color_scheme = "Catpuccin Latte"
 
 -- Opacity for transparency
-config.window_background_opacity = 0.7
+config.window_background_opacity = 0.75
 
 -- Disable tab bar
 config.enable_tab_bar = false
@@ -17,17 +17,61 @@ config.window_padding = {
 	top = 10,
 	bottom = 10,
 }
-
+--SauceCodePro NF SemiBold
 -- Font
-config.font = wezterm.font("LigaSauceCodePro Nerd Font", {
-	weight = "Bold", -- use DemiBold instead of Semibold
-	italic = false, -- true if you want italic
+-- config.font = wezterm.font("LigaSauceCodePro Nerd Font", {
+-- 	weight = "DemiBold",
+-- 	italic = false,
+-- })
+--
+--
+
+
+config.font = wezterm.font_with_fallback({
+	{
+		family = "LigaSauceCodePro Nerd Font",
+		weight = "Medium",
+		italic = false,
+	},
+	"Noto Color Emoji",
 })
-config.font_size = 11.5
+config.font_size = 12
+
+-- 2. The "Ghostty Brightness" Fix (Valid for your version)
+config.foreground_text_hsb = {
+  hue = 1.0,
+  saturation = 1.0,
+  brightness = 1.25, -- Increases white intensity
+}
+
+-- 3. Rendering Engine
+config.front_end = "OpenGL"
+
 
 -- Cursor
 config.default_cursor_style = "BlinkingBar"
 
+-- Smooth cursor animation
+config.animation_fps = 60
+config.cursor_blink_ease_in = 'Constant'
+config.cursor_blink_ease_out = 'Constant'
+
 config.term = "xterm-256color"
 
+-- wezterm.on("user-var-changed", function(window, pane, name, value)
+-- 	userVars = pane:get_user_vars()
+-- 	overrides = window:get_config_overrides() or {}
+-- 	print(overrides)
+-- 	if string.match(userVars["WEZTERM_PROG"], "nvim") then
+-- 		overrides.font = wezterm.font({
+-- 			family = "Iosevka Nerd Font", -- The Font in Nvim
+-- 		})
+-- 		overrides.font_size = 12.0 -- change font size for Neovim
+-- 	else
+-- 		overrides = nil
+-- 	end
+-- 	window:set_config_overrides(overrides)
+-- end)
+
+config.scrollback_lines = 2000
 return config
